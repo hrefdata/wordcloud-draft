88b6a123da02c2d5eeb48fff94bf8c9519db1043
SonHyeonoSonny <oktlrp7624583@gmail.com>
2022-02-24 19:17:39 +0900

    Update project
    부족한 점:
    1. x, y 좌표 랜덤하게 배분
    2. display를 100으로 변경시 글자들이 겹쳐지는 문제점.

--- public/script.js
+++ public/script.js
@@ -1,87 +1,50 @@
-//const { parse } = require("dotenv");
+
 
 const container = document.querySelector(".container");
 const formBox = document.querySelector(".box");
 const wordArea = document.querySelector("#word");
-const smallContainer = document.querySelector("#smallContainer");
-
-// const textAreaArray = document.querySelectorAll('textarea');
-// console.log(textAreaArray);
+// const smallContainer = document.querySelector("#smallContainer");
 
 let words = [];
-// 변수 네이밍 컨벤션, 도메인과 관련된 용어 정의
-
-// source : 번역할 텍스트와 관련된 명칭
-// target : 번역된 결과와 관련된 명칭
-
-// const [sourceTextArea, targetTextArea] = textAreaArray;
 
 // 시작 좌표
-let startPoint = {
-  x: smallContainer.offsetWidth / 2,
-  y: smallContainer.offsetHeight / 2,
-};
-console.log(startPoint);
+// let startPoint = {
+//   x: smallContainer.offsetWidth ,
+//   y: smallContainer.offsetHeight/2,
+// };
+// console.log(startPoint, " dasfdsanfnsaldnflansldnlasndlknfslnfdlsnl");
 
 let wordsDown = [];
 
-const options = words.map((word) => {
-  return {
-    word: word,
-    freq: Math.floor(Math.random() * 50),
-    rotate: (~~(Math.random() * 6) - 3) * 30,
-  };
-});
+// const options = words.map((word) => {
+//   return {
+//     word: word,
+//     freq: Math.floor(Math.random() * 50),
+//     rotate: (~~(Math.random() * 6) - 3) * 30,
+//   };
+// });
 
 // freq 가 큰 것 부터 정렬
-options.sort((a, b) => -1 * (a.freq - b.freq));
+//options.sort((a, b) => -1 * (a.freq - b.freq));
 
-console.log(options);
+//console.log(options);
 //console.log(options[0].word);
 
 // form 삭제 후 div(cloud) 생성 메소드
-const deleteForm = () => {
-  formBox.remove();
-  const cloud = document.createElement("div");
 
-  placeWords();
-};
 
-// div(word) 생성 메소드
-const createWordObject = (word, freq) => {
-  const wordContainer = document.createElement("div");
-  wordContainer.style.position = "absolute";
-  wordContainer.appendChild(document.createTextNode(word));
-  wordContainer.style.lineHeight = 0.8;
-  wordContainer.style.fontSize = freq + "px";
-  wordContainer.style.paddingTop = "10px";
-  wordContainer.style.textAnchor = "middle";
-  // wordContainer.style.transform =
-  //   "translate(300px, 200px) rotate(" + rotate + "deg)";
 
-  // freq 따라서 랜덤 색? 0~10 10~20 20~30 30~40 40~50
-  if (freq > 0 && freq < 3) {
-    wordContainer.style.color = "red";
-  } else if (freq >= 3 && freq < 20) {
-    wordContainer.style.color = "blue";
-  } 
-
-  return wordContainer;
-};
 
 //
-const placeWord = (word, x, y) => {
-  smallContainer.appendChild(word);
-  wordsDown.push(word.getBoundingClientRect());
-  console.log(x, y);
-};
+
 
 //
 const intersect = (word, x, y) => {
   cloud.appendChild(word);
-
+  
   word.style.left = x - word.offsetWidth / 2 + "px";
   word.style.top = y - word.offsetHeight / 2 + "px";
+
 
   var currentWord = word.getBoundingClientRect();
 
@@ -109,16 +72,19 @@ const intersect = (word, x, y) => {
   return false;
 };
 
+const arrX = [180, 190 , 200, 300, 400, 350, 405, 234, 345, 168];
+const arrY = [180, 190 , 200, 300, 400, 350, 405, 234, 345, 168];
 /*  ========================================================== */
-
 const placeWords = () => {
-  console.log(arrVal);
-console.log(arrKey);
-  for (let i = 0; i < 20; i += 1) {
+//   console.log(arrVal);
+// console.log(arrKey);
+  for (let i = 0; i < 10; i += 1) {
+    
     
     let word = createWordObject(
-      arrKey,
-     arrVal,
+      arrKey[i],
+      arrVal[i],
+      (~~(Math.random() * 6) - 3) * 30
       
     );
     // console.log(word);
@@ -128,10 +94,22 @@ console.log(arrKey);
     //   y = (1 + angle) * Math.sin(angle);
     //   placeWord(word, startPoint.x + x, startPoint.y + y);
     // }
-    placeWord(word, startPoint.x, startPoint.y);
+    // console.log(typeof word, word, " eeeeeeeeeeeeeeeeeee");
+    if(word.innerText != "undefined"){
+      placeWord(word, arrX[i], arrY[i]);
+      // intersect(word, startPoint.x, startPoint.y);
+    }
+    
   }
   //console.log(word);
   // placeWord(word, startPoint.x + x, startPoint.y + y);
+};
+
+const deleteForm = () => {
+  formBox.remove();
+  //const cloud = document.createElement("div");
+
+  placeWords();
 };
 
 
@@ -269,3 +247,54 @@ formBox.addEventListener("submit", (event) => {
     }
   }, 500);
 });
+
+
+// div(word) 생성 메소드
+ const createWordObject = (word, freq, rotate) => {
+  const wordContainer = document.createElement("div");
+  //wordContainer.style.position = "relative";
+  wordContainer.appendChild(document.createTextNode(word));
+  //wordContainer.style.lineHeight = 0.8;
+  wordContainer.style.fontSize = freq + "px";
+  wordContainer.style.transform = "translate(100px, 100px) rotate(" + rotate + "deg)";
+  wordContainer.style.textAnchor = "middle";
+  wordContainer.style.alignContent = "middle middle";
+  wordContainer.style.listStyle = "none";
+  wordContainer.style.paddingLeft = "0";
+  wordContainer.style.display = "flex";
+  wordContainer.style.flexWrap = "wrap";
+  wordContainer.style.alignItems = "center";
+  wordContainer.style.justifyContent = "center";
+  wordContainer.style.lineHeight = "2.5rem";
+
+  // wordContainer.style.transform =
+  //   "translate(300px, 200px) rotate(" + rotate + "deg)";
+
+  // freq 따라서 랜덤 색? 0~10 10~20 20~30 30~40 40~50
+  if (freq > 0 && freq < 3) {
+    wordContainer.style.color = "white";
+  } else if (freq >= 3 && freq < 5) {
+    wordContainer.style.color = "black";
+  } else if (freq >= 5 && freq < 8) {
+    wordContainer.style.color = "purple";
+  } else if (freq >= 8 && freq < 10) {
+    wordContainer.style.color = "red";
+  }else if (freq >= 10 && freq < 20) {
+    wordContainer.style.color = "green";
+  } else {
+    wordContainer.style.color = "blue";
+  }
+
+  return wordContainer;
+};
+
+const placeWord = (word, x, y) => {
+  // console.log(typeof word, word, "dddddddddddddddddddddddddddddd");
+ 
+  // if(word){
+    container.appendChild(word);
+    wordsDown.push(word.getBoundingClientRect());
+
+  // }
+  //console.log(x, y);
+};
